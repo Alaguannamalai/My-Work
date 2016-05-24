9bed66565b077425c5c453bade8eb75fd451fc98
Alaguannamalai Alagappan <aalaguannamalai@gmail.com>
2016-05-25 02:41:40 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,16 @@
+package amazon;
+
+public class ArrayDemo {
+	
+	public static void main(String args[]){
+		int number[] = new int[10];  /* number is an array of 10 integers */
+		int i=0;
+		
+		while(i<10){
+			number[i] = i+100;
+			// format is a funtion to arrange ouotput in the below given format
+			System.out.format("number[%d] = %d\n", i, number[i]);
+			i = i+1;
+		}
+	}
+}
